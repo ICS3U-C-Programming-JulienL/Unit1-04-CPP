361a210f62ab4e5e8f7c5faaf45f07a39e792e31
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-09-18 21:26:05 +0000

    Added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 Julien Lamoureux All rights reserved.
+// .
+// Created by: Julien Lamoureux
+// Date: September 18, 2023
+// This program finds the area and perimeter of a rectangle in C++
+
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "The dimensions of a rectangle are:" << std::endl;
+    std::cout << "6 cm x 2 cm" << std::endl;
+    std::cout << "The area is " << (6 * 2) << " cm^2" << std::endl;
+    std::cout << "The perimeter is " << (2 * (6 + 2)) << " cm" << std::endl;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 Julien Lamoureux All rights reserved.
+// .
+// Created by: Julien Lamoureux
+// Date: September 18, 2023
+// This program does basic math in C++
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "8+2= " << (8 + 2) << std::endl;
+    std::cout << "8-2= " << (8 - 2) << std::endl;
+    std::cout << "8*2= " << (8 * 2) << std::endl;
+    std::cout << "8/3= " << (8 / 3) << std::endl;
+    std::cout << "8**3= " << (pow(8, 3)) << std::endl;
+    std::cout << "8**0.5= " << (pow(8, 0.5)) << std::endl;
+}
